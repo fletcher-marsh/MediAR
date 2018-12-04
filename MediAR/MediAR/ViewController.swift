45ab17a08c2cb42e48c45feb155a1dc4887a9e50
Fletcher Marsh <fletcher.r.marsh@gmail.com>
2018-12-03 19:39:04 -0500

    Button edits

--- MediAR/MediAR/ViewController.swift
+++ MediAR/MediAR/ViewController.swift
@@ -100,7 +100,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-//        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
         // Pause the view's session
         sceneView.session.pause()
     }
